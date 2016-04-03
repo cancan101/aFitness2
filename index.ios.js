a0101b409e404168ff5096afca6bef7a574f5728
Alex Rothberg <agrothberg@gmail.com>
2016-04-02 21:09:50 -0400

    Initial work on IOS

--- index.ios.js
+++ index.ios.js
@@ -8,23 +8,25 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
+import Exercises, { loadData } from './components/Exercises';
+
+
 class aFitness2 extends Component {
+  _onPressButton = () => {
+    loadData();
+    this.forceUpdate();
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={[styles.container, {paddingTop: 20,}]}>
+        <TouchableHighlight onPress={this._onPressButton}>
+          <Text>Load Data</Text>
+        </TouchableHighlight>
+        <Exercises ref="exercises" />
       </View>
     );
   }
@@ -33,19 +35,6 @@ class aFitness2 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
