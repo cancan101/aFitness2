90d16b2909ec72b24603698b4c886eccba80337c
Alex Rothberg <agrothberg@gmail.com>
2016-05-11 02:23:10 -0400

    WIP

--- components/Exercises/index.js
+++ components/Exercises/index.js
@@ -3,21 +3,15 @@ import React, {
 } from 'react';
 import {
   Alert,
-  Image,
   Picker,
-  Text,
   View,
 } from 'react-native';
 
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Listitem from 'react-native-listitem';
-import ListitemStyles from 'react-native-listitem/styles';
 import { ListView } from 'realm/react-native';
 
-import { MainRouter } from '../../routers';
-
-import IMAGES from '../../constants/Images';
 import realm from '../../realm';
+import ExerciseList from './ExerciseList';
 
 
 const FILTER_ALL = 'all';
@@ -35,7 +29,6 @@ export default class Exercises extends Component {
   static title = 'Exercises';
   static iconName = 'view-carousel';
 
-
   static propTypes = {
     navigator: React.PropTypes.object.isRequired,
   };
@@ -49,38 +42,6 @@ export default class Exercises extends Component {
     };
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
-
-  _renderRow = (exercise) => {
-    const route = MainRouter.getExerciseRoute(exercise);
-
-    let image;
-    if (exercise.image) {
-      image = (
-        <Image
-          source={IMAGES[exercise.image]}
-          style={{ width: 40, height: 40, marginBottom: -15, marginTop: -15 }}
-        />
-      );
-    } else {
-      image = (
-        <View
-          style={{ width: 40, height: 40, marginBottom: -15, marginTop: -15 }}
-        />
-      );
-    }
-
-    return (
-      <Listitem
-        onPress={() => this.props.navigator.push(route)}
-      >
-        <View style={{ flexDirection: 'row' }}>
-          {image}
-          <Text style={ListitemStyles.liText}>{exercise.name}</Text>
-        </View>
-      </Listitem>
-    );
-  };
-
   _selectMuscleGroup = (selectedMuscleGroup) => {
     this.setState({
       selectedMuscleGroup,
@@ -148,11 +109,10 @@ export default class Exercises extends Component {
     exercisesFiltered = exercisesFiltered.sorted('name');
 
     return (
-      <ListView
-        enableEmptySections
+      <ExerciseList
+        {...this.props}
         renderHeader={this._renderHeader}
         dataSource={this._ds.cloneWithRows(exercisesFiltered)}
-        renderRow={this._renderRow}
       />
     );
   }
